5924bd71f13729a50033b832e39e2db45fc0315a
AniketSindhu <sindhuaniket@gmail.com>
2019-11-11 01:52:30 +0530

    made obstacles 's speed varry with score

---
+++ Reflexes Test/Assets/Scripts/Lvl 2/flags.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class flags : MonoBehaviour
+{   
+    public static int score=0;
+   public float initialspeed=3.5f;
+   public float speed=0f;
+   float multiplier=0.1f;
+    void Update()
+    {   speed=initialspeed+score*multiplier;
+        transform.Translate(Vector2.left*speed*Time.deltaTime);
+    }
+         public void OnTriggerEnter2D(Collider2D other) 
+        {    
+         if (other.CompareTag("Player"))
+        {Debug.Log(score);
+        SceneManager.LoadScene(2);
+        speed=0f;
+        }
+        else if(other.tag==gameObject.tag)
+        {       
+            Destroy(other.gameObject);
+            Destroy (gameObject);
+            score++;
+            
+        }
+
+        else
+        {
+            Debug.Log(score);           
+            SceneManager.LoadScene("Gameover");
+            speed=0f;
+        }
+        
+    
+    }
+}
+
---
+++ Reflexes Test/Assets/Scripts/Lvl 2/flags_spawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class flags_spawn : MonoBehaviour
+
+{
+    public float First_Spawn;
+    public float timebtSpawn=0.7f;
+
+    public GameObject[] flags;
+    
+void Start () 
+{ Scene currentscene=SceneManager.GetActiveScene();
+  string m=currentscene.name;              
+    if(m=="Level 2")
+     InvokeRepeating ("spawn", First_Spawn,timebtSpawn);
+}
+
+     void spawn()
+    {
+        int rand=Random.Range(0,flags.Length);
+        Instantiate(flags[rand],transform.position,Quaternion.identity);
+            
+    }
+      
+    }
+
